52980760f078a8fb1a6a3ed2abaa228b2b4efddf
Aditya_Kumar_Ray <rayaditya414@gmail.com>
2022-08-07 21:02:29 +0530

    App root folder ready for use

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return ( 
+        <div className="App">
+            <h1>Lets get started</h1>
+            <div className="app__header">
+                
+            </div>
+        </div>
+    );
+}
+export default App;
